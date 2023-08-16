a931ca86df820290ad4ad81cafb3e27a38c550d1
Avently <7953703+avently@users.noreply.github.com>
2023-08-16 16:50:30 +0800

    test

--- apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/platform/PlatformTextField.desktop.kt
+++ apps/multiplatform/common/src/desktopMain/kotlin/chat/simplex/common/platform/PlatformTextField.desktop.kt
@@ -54,7 +54,7 @@ actual fun PlatformTextField(
   var textFieldValueState by remember { mutableStateOf(TextFieldValue(text = cs.message)) }
   val textFieldValue = textFieldValueState.copy(text = cs.message)
   val clipboard = LocalClipboardManager.current
-  BasicTextField(
+  TextField(
     value = textFieldValue,
     onValueChange = {
       if (!composeState.value.inProgress && !(composeState.value.preview is ComposePreview.VoicePreview && it.text != "")) {
@@ -62,85 +62,6 @@ actual fun PlatformTextField(
         onMessageChange(it.text)
       }
     },
-    textStyle = textStyle.value,
-    maxLines = 16,
-    keyboardOptions = KeyboardOptions.Default.copy(
-      capitalization = KeyboardCapitalization.Sentences,
-      autoCorrect = true
-    ),
-    modifier = Modifier
-      .padding(vertical = 4.dp)
-      .focusRequester(focusRequester)
-      .onPreviewKeyEvent {
-        if (it.key == Key.Enter && it.type == KeyEventType.KeyDown) {
-          if (it.isShiftPressed) {
-            val start = if (minOf(textFieldValue.selection.min) == 0) "" else textFieldValue.text.substring(0 until textFieldValue.selection.min)
-            val newText = start + "\n" +
-                  textFieldValue.text.substring(textFieldValue.selection.max, textFieldValue.text.length)
-            textFieldValueState = textFieldValue.copy(
-              text = newText,
-              selection = TextRange(textFieldValue.selection.min + 1)
-            )
-            onMessageChange(newText)
-          } else if (cs.message.isNotEmpty()) {
-            onDone()
-          }
-          true
-        } else if (it.key == Key.DirectionUp && it.type == KeyEventType.KeyDown && cs.message.isEmpty()) {
-          onUpArrow()
-          true
-        } else if (it.key.nativeKeyCode == 16778305 /*it.key == Key.C*/ && it.type == KeyEventType.KeyDown && it.isMetaPressed && desktopPlatform.isMac()) {
-          if (textFieldValue.selection.min != textFieldValue.selection.max) {
-            clipboard.setText(AnnotatedString(textFieldValue.getSelectedText().text))
-          }
-          true
-        } else if (it.key.nativeKeyCode == 16778311 /*it.key == Key.X*/ && it.type == KeyEventType.KeyDown && it.isMetaPressed && desktopPlatform.isMac()) {
-          if (textFieldValue.selection.min != textFieldValue.selection.max) {
-            clipboard.setText(AnnotatedString(textFieldValue.getSelectedText().text))
-            val newText = textFieldValue.getTextBeforeSelection(1_000_000) + textFieldValue.getTextAfterSelection(1_000_000)
-            textFieldValueState = textFieldValue.copy(
-              annotatedString = newText,
-              selection = TextRange(textFieldValue.selection.min)
-            )
-            onMessageChange(newText.text)
-          }
-          true
-        } else if (it.key.nativeKeyCode == 16778300/*it.key == Key.V*/ && it.type == KeyEventType.KeyDown && it.isMetaPressed && desktopPlatform.isMac()) {
-          val clipboardText = clipboard.getText()
-          if (clipboardText != null) {
-            val newText = textFieldValue.getTextBeforeSelection(1_000_000) + clipboardText +
-                textFieldValue.getTextAfterSelection(1_000_000)
-            textFieldValueState = textFieldValue.copy(
-              annotatedString = newText,
-              selection = TextRange(textFieldValue.selection.min + clipboardText.length)
-            )
-            onMessageChange(newText.text)
-          }
-          true
-        } else false
-      },
-    cursorBrush = SolidColor(MaterialTheme.colors.secondary),
-    decorationBox = { innerTextField ->
-      Surface(
-        shape = RoundedCornerShape(18.dp),
-        border = BorderStroke(1.dp, MaterialTheme.colors.secondary)
-      ) {
-        Row(
-          Modifier.background(MaterialTheme.colors.background),
-          verticalAlignment = Alignment.Bottom
-        ) {
-          CompositionLocalProvider(
-            LocalLayoutDirection provides if (isRtl) LayoutDirection.Rtl else LocalLayoutDirection.current
-          ) {
-            Column(Modifier.weight(1f).padding(start = 12.dp, end = 32.dp)) {
-              Spacer(Modifier.height(8.dp))
-              innerTextField()
-              Spacer(Modifier.height(10.dp))
-            }
-          }
-        }
-      }
-    }
   )
   showDeleteTextButton.value = cs.message.split("\n").size >= 4 && !cs.inProgress
   if (composeState.value.preview is ComposePreview.VoicePreview) {
